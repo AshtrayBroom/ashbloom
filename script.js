0645e724ccd26fa6e882fd57bf39ed84e5844883
Ashley <ashtraybroom@gmail.com>
2016-03-15 15:14:04 +1100

    Same nav bar across pages

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("navMenu").innerHTML =
+ '<ul>'+
+  '<li><a href="Index.html">HOME</a></li>'+
+  '<li><a href="about.html">ABOUT</a></li>'+
+  '<li><a href="ideas.html">IDEAS</a></li>'+
+  '<li><a href="contact.html">CONTACT</a></li>'+
+ '</ul>';
